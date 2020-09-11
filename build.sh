20dabf34468f51ee99a399632742c980577f6aa4
starudream <justwangsheng@qq.com>
2020-09-11 15:00:50 +0800

    feat: update

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 
 NAME=$1
 
-cd "./${NAME}"
+cd "./${NAME}" || exit 1
 
 echo -e "\033[34m[ Start '${NAME}' ]\033[0m"
 
@@ -20,12 +20,6 @@ docker login -u "${USERNAME}" -p "${PACKAGE_TOKEN}" docker.pkg.github.com
 echo -e "\033[32m[ Publish GitHub Package ]\033[0m"
 docker tag "${USERNAME}"/"${NAME}":latest docker.pkg.github.com/"${GITHUB_REPOSITORY}"/"${NAME}":latest
 docker push docker.pkg.github.com/"${GITHUB_REPOSITORY}"/"${NAME}":latest
-
-echo -e "\033[32m[ Login Aliyun Docker ]\033[0m"
-docker login -u "${ALIYUN_DOCKER_USERNAME}" -p "${ALIYUN_DOCKER_TOKEN}" registry.cn-shanghai.aliyuncs.com
-echo -e "\033[32m[ Publish Aliyun Docker ]\033[0m"
-docker tag "${USERNAME}"/"${NAME}":latest registry.cn-shanghai.aliyuncs.com/"${USERNAME}"/"${NAME}":latest
-docker push registry.cn-shanghai.aliyuncs.com/"${USERNAME}"/"${NAME}":latest
 
 echo -e "\033[34m[ End '${NAME}' ]\033[0m"
 
