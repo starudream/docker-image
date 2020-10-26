d25f315325b6d5da7f8480ec2a8fcfb82789f787
starudream <justwangsheng@qq.com>
2020-10-26 15:56:15 +0800

    fix: fix

--- build.sh
+++ build.sh
@@ -15,12 +15,6 @@ docker login -u "${USERNAME}" -p "${DOCKER_TOKEN}"
 echo -e "\033[32m[ Publish Docker Hub ]\033[0m"
 docker push "${USERNAME}"/"${NAME}":latest
 
-echo -e "\033[32m[ Login GitHub Package ]\033[0m"
-docker login -u "${USERNAME}" -p "${PACKAGE_TOKEN}" docker.pkg.github.com
-echo -e "\033[32m[ Publish GitHub Package ]\033[0m"
-docker tag "${USERNAME}"/"${NAME}":latest docker.pkg.github.com/"${USERNAME}"/"${REPOSITORY}"/"${NAME}":latest
-docker push docker.pkg.github.com/"${USERNAME}"/"${REPOSITORY}"/"${NAME}":latest
-
 echo -e "\033[34m[ End '${NAME}' ]\033[0m"
 
 cd "../"
