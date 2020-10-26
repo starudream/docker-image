ba8cf820307d040b4dd67b0975ede9033110f09b
starudream <justwangsheng@qq.com>
2020-10-26 15:49:20 +0800

    feat: update

--- build.sh
+++ build.sh
@@ -18,8 +18,8 @@ docker push "${USERNAME}"/"${NAME}":latest
 echo -e "\033[32m[ Login GitHub Package ]\033[0m"
 docker login -u "${USERNAME}" -p "${PACKAGE_TOKEN}" docker.pkg.github.com
 echo -e "\033[32m[ Publish GitHub Package ]\033[0m"
-docker tag "${USERNAME}"/"${NAME}":latest docker.pkg.github.com/"${GITHUB_REPOSITORY}"/"${NAME}":latest
-docker push docker.pkg.github.com/"${GITHUB_REPOSITORY}"/"${NAME}":latest
+docker tag "${USERNAME}"/"${NAME}":latest docker.pkg.github.com/"${USERNAME}"/"${REPOSITORY}"/"${NAME}":latest
+docker push docker.pkg.github.com/"${USERNAME}"/"${REPOSITORY}"/"${NAME}":latest
 
 echo -e "\033[34m[ End '${NAME}' ]\033[0m"
 
